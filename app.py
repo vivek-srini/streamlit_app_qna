5941a31d2e2839e9b85d2e008176ca36cbc00161
vivek-srini <38190456+vivek-srini@users.noreply.github.com>
2023-07-07 16:43:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def langchain_response(texts,embeddings,question,prompt_template,k):
             
     prompt = PromptTemplate(template=prompt_template, input_variables=['context',"question"])
     type_kwargs = {"prompt": prompt}
-    retriever = db.as_retriever(search_type="similarity", search_kwargs={"k":k})
+    retriever = db.as_retriever(search_type="similarity", search_kwargs={"k":int(k)})
     qa = RetrievalQA.from_chain_type(llm=ChatOpenAI(temperature=0,max_tokens=600,openai_api_key = openai_api_key), chain_type="stuff",retriever=retriever, chain_type_kwargs=type_kwargs)
     result = qa({"query": question})
     return result["result"]
