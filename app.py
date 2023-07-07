9f68773397c2541df67cfc176b7e3c73ac5126ac
vivek-srini <38190456+vivek-srini@users.noreply.github.com>
2023-07-07 16:11:46 +0530

    Update app.py
    prompt

--- app.py
+++ app.py
@@ -31,9 +31,8 @@ def translate_tamil_to_english(text):
 def langchain_response(texts,embeddings,question):
     db = FAISS.from_texts(texts, embeddings)
     prompt_template = """Train yourself on provided data as below to answer questions and summarize the 
-            output in bullet points capturing full context. Think step by step before generating the response. 
-            If you don't find an answer in the training dataset, then give a blank output. Answer keeping in mind the context 
-            of question asked
+            output capturing full context. Think step by step before generating the response. 
+            Please dont miss out any information that is relevant. Include all related information in the answer
             context: {context}
             question: {question}
             Helpful Answer: """
