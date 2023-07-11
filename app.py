5ce7efad80e4119dc1f4c955069d4f263dd8b136
vivek-srini <38190456+vivek-srini@users.noreply.github.com>
2023-07-11 20:18:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -157,9 +157,10 @@ def main():
             audio_bytes = audio_recorder()
             if audio_bytes:
                 st.audio(audio_bytes, format="audio/wav")
-                user_question = transcript_audio(audio_bytes)
+        if require_audio and audio_bytes:
+          user_question = transcript_audio(audio_bytes)
         else:
-            user_question = st.text_input("Ask a question about your PDF:")
+          user_question = st.text_input("Ask a question about your PDF:")
         if user_question and k:
           
           if selected_language == 'Tamil':
